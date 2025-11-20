f1717e0148fe9a3c699e5077c2a9c2b5716cc50e
winstonpgao <leon20130301@hotmail.com>
2025-11-20 15:09:40 +1100

    configure gor pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/arcsage-web/',
+  build: {
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
 })
